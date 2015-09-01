ad971ea48d053e6263d9fe5f49bc1beb0b19dcfe
Plamena Miteva <plamenamiteva@abv.bg>
2015-09-01 14:23:37 +0300

    BaseApiController
    BaseApiController added

---
+++ Twitter.REST/Twitter.Services/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿namespace SocialNetwork.Services.Controllers
+{
+    using System.Web.Http;
+    using Twitter.Data;
+
+    public class BaseApiController : ApiController
+    {
+        public BaseApiController()
+            : this(new TwitterContext())
+        {
+        }
+
+        public BaseApiController(TwitterContext data)
+        {
+            this.Data = data;
+        }
+
+        protected TwitterContext Data { get; set; }
+    }
+}
